3ba464e6af6ca26b122d2280279a79ba93eed644
Daemon <69844188+daemon-reconfig@users.noreply.github.com>
2021-10-13 12:12:17 +0530

    Add files via upload

---
+++ screens/5acb05c1-07cb-48a1-b2b8-aec7f4044e18-1634105924640.js
@@ -0,0 +1,40 @@
+jQuery("#simulation")
+  .on("click", ".s-5acb05c1-07cb-48a1-b2b8-aec7f4044e18 .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(jimUtil.isAlternateModeActive()) return;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Button_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimCreateData",
+                  "parameter": {
+                    "datatype": "datamaster",
+                    "datamaster": "Name",
+                    "fields": {
+                      "c98c9a40-1ecc-40a1-88f6-1fbe683d63ce": {
+                        "datatype": "property",
+                        "target": "#s-Input_1",
+                        "property": "jimGetValue"
+                      }
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
